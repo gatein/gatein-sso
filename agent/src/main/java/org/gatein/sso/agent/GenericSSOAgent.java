5afa9e76382952a5863c7f775f0e70364655c6e8
Sohil Shash <openmobster@gmail.com>
2010-04-13 21:24:07 +0000

    GTNPORTAL-1027 - GateIn+SSO integration: Blank screen when SSO ticket is not valid (OpenSSO)

--- agent/src/main/java/org/gatein/sso/agent/GenericSSOAgent.java
+++ agent/src/main/java/org/gatein/sso/agent/GenericSSOAgent.java
@@ -70,7 +70,16 @@ public class GenericSSOAgent extends InitiateLoginServlet
 	{
 		try
 		{
-			this.processSSOToken(req,resp);		
+			this.processSSOToken(req,resp);	
+			
+			String portalContext = req.getContextPath();
+			if(req.getAttribute("abort") != null)
+			{
+				String ssoRedirect = portalContext + "/sso";
+				resp.sendRedirect(ssoRedirect);
+				return;
+			}
+			
 			super.doGet(req, resp);
 		}
 		catch(Exception e)
@@ -105,8 +114,16 @@ public class GenericSSOAgent extends InitiateLoginServlet
 		}
 		else
 		{
-			//See if an OpenSSO Token was used
-			OpenSSOAgent.getInstance(this.ssoServerUrl, this.ssoCookieName).validateTicket(httpRequest);
+			try
+			{
+				//See if an OpenSSO Token was used
+				OpenSSOAgent.getInstance(this.ssoServerUrl, this.ssoCookieName).validateTicket(httpRequest);
+			}
+			catch(IllegalStateException ilse)
+			{
+				//somehow cookie failed validation, retry by starting the opensso login process again
+				httpRequest.setAttribute("abort", Boolean.TRUE);
+			}
 		}
 	}		
 }
